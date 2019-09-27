a6c216eb07681225d4493b12d5ebc14ffdc34cca
kacoro <kacoro@qq.com>
2019-09-27 10:18:13 +0800

    fixed json and support css

--- index.js
+++ index.js
@@ -11,19 +11,22 @@ fs = require("fs"),
 { getFiles } = require('./utils/dir');
 
 program
-  .version("1.0.0")
-  .option("-t, --test [type]", "set source and taget directory to ./test/")
-  .option("-s, --set [type]", "set source and taget  by user input")
+  .version("1.0.1")
+  .option("-t, --test [type]", "set source and target directory to ./test/")
+  .option("-s, --set [type]", "set source and target  by user input")
   .option("-d, --drop [type]", "drop blac background")
+  .option("-p, --padding [value]", "set the padding output, default is 0")
+  .option("-n, --name [value]", "set the output name, default is sprites")
   .parse(process.argv);
 
 var imgMetadataList = [];
-var imageList = [];
 var maxWidth = 0;
 var maxHeight = 0;
+var padding = 0;
+var targetName = 'sprites'
 var blankImage= {
   path:path.join(__dirname,"./blank.png"),
-  taget:"sprites.png",
+  target:targetName+".png",
   image:null,
   width:0,
   height:0
@@ -42,19 +45,38 @@ const questions = [
 ];
 
 var str = ''
-
-function makeJson(source,x,y){
-  var reg = /\.*?(bmp|gif|png|jpg)/gi;
+var htmlStr = ''
+var cssStr = ''
+var frame = [];
+function makeJson(source,x,y,i,j){
+  var reg = /\.*?(bmp|gif|png|jpg|jpeg)/gi;
   var name = source.name.replace(reg, '');
   var tpl = `"${name}":{
-    "frame": {"x":${x},"y":${y},"w":${maxWidth},"h":${maxHeight}},
+    "frame": {"x":${maxWidth * i},"y":${maxHeight * j},"w":${maxWidth},"h":${maxHeight}},
    "rotated": false,
-    "trimmed": false,
+    "trimmed": true,
     "spriteSourceSize": {"x":0,"y":0,"w":${source.bitmap.width},"h":${source.bitmap.height}},
     "sourceSize": {"w":${source.bitmap.width},"h":${source.bitmap.height}},
     "pivot": {"x":0,"y":0}
-    },`
+    }`
     return tpl;
+}
+
+function makeHtml(index){
+  var tpl = `
+    <div class="icon-${targetName} icon-${targetName}-${index}">
+    </div>
+  `
+  return tpl
+}
+
+function makeCss(index,i,j){
+  var tpl = `
+    .icon-${targetName}-${index}{
+        background-position:-${maxWidth * i}px -${maxHeight * j}px;
+    }
+  `
+  return tpl
 }
 
 const generation = async form => {
@@ -62,10 +84,9 @@ const generation = async form => {
   const inputPath = form.input || "./";
   const outputPath = form.output || "./";
   const input = chalk.green(`${inputPath}`);
-  const output = chalk.green(`${outputPath + blankImage.taget}`);
+  const output = chalk.green(`${outputPath + blankImage.target}`);
   console.log(`images directory is :${input} `); //
   let imageList = getFiles.getImageFiles(inputPath);
-  console.log(imageList)
   for (const name of imageList) {
     const column = await Jimp.read(inputPath + name)
       .then(image => {
@@ -82,7 +103,8 @@ const generation = async form => {
       });
   }
   var sqrt = Math.ceil(Math.sqrt(imgMetadataList.length));
-  
+  maxWidth += padding;
+  maxHeight += padding;
   await Jimp.read(blankImage.path).then(images => {
     console.log(maxWidth * sqrt, maxHeight * sqrt);
     var num = sqrt;
@@ -101,7 +123,13 @@ const generation = async form => {
     x = maxWidth * i + (maxWidth - item.bitmap.width) /2;
     y = maxHeight * j + (maxHeight - item.bitmap.height) /2;
    
-    str += makeJson(item,x,y)
+   if(index == imgMetadataList.length-1){
+    str += makeJson(item,x,y,i,j) 
+   }else{
+    str += makeJson(item,x,y,i,j)+','
+   }
+    htmlStr += makeHtml(index)
+    cssStr += makeCss(index,i,j)
     blankImage.image.composite(item, x, y);//拼接图片
     i++;
     if (i+1 > sqrt) {
@@ -128,28 +156,52 @@ const generation = async form => {
         blankImage.image.bitmap.data[idx + 2] = replaceColor.b;
         blankImage.image.bitmap.data[idx + 3] = replaceColor.a;
       }
-    }).write(outputPath + blankImage.taget);
+    }).write(outputPath + blankImage.target);
   }else{
-    blankImage.image.background(Jimp.rgbaToInt(255, 255, 255, 1)).write(outputPath + blankImage.taget);
+    blankImage.image.write(outputPath + blankImage.target);
   }
   str = `
   {
-    "frames": { ${str}},
+    "frames": {${str}},
     "meta": {
       "app": "http://www.kacoro.com/",
       "version": "1.0.0",
-      "image": "${blankImage.taget}",
+      "image": "${blankImage.target}",
       "format": "RGBA8888",
       "size": {"w":${blankImage.image.bitmap.width},"h":${blankImage.image.bitmap.height}},
       "scale": "1",
-      "smartupdate": "$TexturePacker:SmartUpdate:5dfda9c839d3ca51c00faf9458ba1fca:0b0be3727c36efcd76ae8acf6abbf211:81fc68276d1596f8f7ad75d59a9ce9b5$"
+      "smartupdate": ""
     }
   }
   `
-  fs.writeFile(outputPath+'sprites.json',str,function(err){
+  htmlStr = `
+  <html>
+  <head>
+    <link rel="stylesheet" href="./${targetName}.css">
+  </head>
+    <body>
+       ${htmlStr}
+    </body>
+  </html>
+  `
+  cssStr = `
+    .icon-${targetName}{
+       background:url("${targetName}.png") no-repeat;
+       width:${maxWidth}px;
+       height:${maxHeight}px;
+       display:inline-block;
+    }
+    ${cssStr}
+  `
+  fs.writeFile(outputPath+ targetName+'.json',str,function(err){
     
   })
-  
+  fs.writeFile(outputPath+ targetName+'.html',htmlStr,function(err){
+    
+  })
+  fs.writeFile(outputPath+ targetName+'.css',cssStr,function(err){
+    
+  })
   const ms = new Date() - start;
   console.log(
     `output : ${output},pixel is：${maxWidth * sqrt}x${maxHeight *
@@ -160,6 +212,13 @@ const generation = async form => {
 console.log(program)
 if(program.drop == true){
 
+}
+if(program.padding){
+   padding = program.padding
+}
+if(program.name){
+  targetName = program.name
+  blankImage.traget = targetName + '.png'
 }
 if (program.test == true) { //test的话直接引用
   console.log("test")
